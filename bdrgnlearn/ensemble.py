127b805cc3bb544c59f993f3ff8022727e85e8f7
bdrgn <bdrgn@yandex.ru>
2021-03-02 23:40:49 +0300

    Addded Gradient Boosting.

--- bdrgnlearn/ensemble.py
+++ bdrgnlearn/ensemble.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from bdrgnlearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
 
+
 class RandomForestClassifier:
     # Initialize the object entity
     def __init__(self, min_samples_leaf=1, max_depth=None, n_trees=10, threshold=0.5):
@@ -69,8 +70,8 @@ class RandomForestRegressor:
         for i in range(self.n_trees_):
             # Initialize single tree object
             tree = DecisionTreeRegressor(max_depth=self.max_depth_, min_samples_leaf=self.min_samples_leaf_,
-                                          bootstrap=True, \
-                                          bootstrap_coeff=1 / self.n_trees_)
+                                         bootstrap=True, \
+                                         bootstrap_coeff=1 / self.n_trees_)
 
             # Fit the tree to bootstrapped data
             tree.fit(X, y)
@@ -90,6 +91,97 @@ class RandomForestRegressor:
 
         # Take the average of all trees for each observation in dataset
         predictions = np.mean(trees_predictions, axis=1)
+
+        # Return an array with predictions for each observation
+        return predictions
+
+
+class GradientBoostingRegressor:
+    # Initialize the object entity
+    def __init__(self, min_samples_leaf=1, max_depth=3, n_trees=10, threshold=0.5):
+
+        # Store the input parameters of the class
+        self.max_depth_ = max_depth
+        self.min_samples_leaf_ = min_samples_leaf
+        self.n_trees_ = n_trees
+        self.threshold_ = threshold
+
+    def fit(self, X, y):
+
+        # Initialize a list to store tree objects
+        self.trees = []
+
+        for i in range(self.n_trees_):
+            # Initialize single tree object
+            tree = DecisionTreeRegressor(max_depth=self.max_depth_, min_samples_leaf=self.min_samples_leaf_)
+
+            # Fit the tree to bootstrapped data
+            tree.fit(X, y - self.predict(X))
+
+            # Append tree to a list
+            self.trees.append(tree)
+
+            # Make predictions with current trees
+            y_pred = self.predict(X)
+
+    def predict(self, X):
+
+        # Initialize a list to store the prediction value for each tree
+        trees_predictions = np.empty((len(X), len(self.trees)))
+
+        # Iterate over all observations
+        for i, tree in enumerate(self.trees):
+            trees_predictions[:, i] = tree.predict(X)
+
+        # Take the sum of all trees for each observation in dataset
+        predictions = np.sum(trees_predictions, axis=1)
+
+        # Return an array with predictions for each observation
+        return predictions
+
+
+class GradientBoostingClassifier:
+    # Initialize the object entity
+    def __init__(self, min_samples_leaf=1, max_depth=3, n_trees=10, threshold=0.5):
+
+        # Store the input parameters of the class
+        self.max_depth_ = max_depth
+        self.min_samples_leaf_ = min_samples_leaf
+        self.n_trees_ = n_trees
+        self.threshold_ = threshold
+
+    def fit(self, X, y):
+
+        # Initialize a list to store tree objects
+        self.trees = []
+
+        for i in range(self.n_trees_):
+            # Initialize single tree object
+            tree = DecisionTreeRegressor(max_depth=self.max_depth_, min_samples_leaf=self.min_samples_leaf_)
+
+            # Fit the tree to bootstrapped data
+            tree.fit(X, y - self.predict(X))
+
+            # Append tree to a list
+            self.trees.append(tree)
+
+            # Make predictions with current trees
+            y_pred = self.predict(X)
+
+    def predict(self, X):
+
+        # Initialize a list to store the prediction value for each tree
+        trees_predictions = np.empty((len(X), len(self.trees)))
+
+        # Iterate over all observations
+        for i, tree in enumerate(self.trees):
+            trees_predictions[:, i] = tree.predict(X)
+
+        # Take the sum of all trees for each observation in dataset
+        predictions = np.sum(trees_predictions, axis=1)
+
+        # Get 0 or 1 according to threshold
+        predictions = np.float64(predictions >= self.threshold_)
 
         # Return an array with predictions for each observation
         return predictions
